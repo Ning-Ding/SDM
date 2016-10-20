56f3093bfd4173c587192086fb25d924e30951b3
dingningbuaa <dingning_buaa@foxmail.com>
2016-10-20 14:41:17 +0800

    Update load_all_data.py

--- load_all_data.py
+++ load_all_data.py
@@ -4,10 +4,9 @@ Created on Wed Oct 19 19:23:58 2016
 @author: dingning
 """
 import os
-import numpy as np
 from scipy import io
 from PIL import Image,ImageDraw
-from matplotlib import pyplot
+from pylab import *
 
 #loading ground_truth mat file
 def load_ground_truth_data():
@@ -50,11 +49,32 @@ def crop_image(image_name,bbox,data_type = 'train'):
     input: image_name ex:'image_0122'
            bbox like this: [x1,y1,x2,y2] or [(x1,y1),(x2,y2)]
            data_type maybe 'train' or 'test' by default is 'train'
-    output: a cropped image numpy array
+    output: a tuple containing a cropped image numpy array and it's landmarks
     '''
     image_path = 'data/' + data_type + 'set/png/' + image_name + '.png'
     assert os.path.exists(image_path)
     im = Image.open(image_path)
-    draw = ImageDraw.Draw(im)
-    draw.rectangle(bbox)
-    im.show()
+    bbox = map(lambda x: int(round(x)),bbox)
+    im_crop = im.crop(bbox)
+    marks = load_landmarks(image_name)
+    marks -= bbox[:2]
+    im_array = array(im_crop)
+    return im_array,marks
+    
+    '''
+    imshow(im_crop)
+    plot(marks.T[0],marks.T[1],'b*')
+    show()
+    '''
+
+
+
+if __name__=='__main__':
+    '''
+    by run this python file, we could get a list named Data
+    Data containing 811 tuples and each tuple holds two numpy array containing images and marks
+    '''
+    bbox = load_ground_truth_data()
+    image_list = get_data_path_list()
+    Data = []
+    for path in image_list:Data.append(crop_image(path[:10], list(bbox[path])))
