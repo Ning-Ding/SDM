f25214ca15e906f71137f655557fe9bc48c8632d
dingningbuaa <dingning_buaa@foxmail.com>
2016-10-20 09:41:47 +0800

    Update load_all_data.py

--- load_all_data.py
+++ load_all_data.py
@@ -14,7 +14,7 @@ def load_ground_truth_data():
     key: a string of filename
     value: a numpy array of bounding_box
     '''
-    file_path = 'bounding_boxes/bounding_boxes_lfpw_trainset.mat'
+    file_path = 'data/bounding_boxes/bounding_boxes_lfpw_trainset.mat'
     assert os.path.exists(file_path)
     x = io.loadmat(file_path)['bounding_boxes'][0]
     x = [x[0][0] for x in x]
@@ -25,15 +25,20 @@ def load_landmarks(image_name):
     '''
     output data type: a numpy array containing tuples of points    
     '''   
-    file_path = 'trainset/pts/' + image_name + '.pts'  
+    file_path = 'data/trainset/pts/' + image_name + '.pts'  
     assert os.path.exists(file_path)
     with open(file_path) as f: rows = [rows.strip() for rows in f]
     coords_set = [point.split() for point in rows[rows.index('{') + 1:rows.index('}')]]
     return np.array([tuple([float(point) for point in coords]) for coords in coords_set])
     
 #loading images png file    
-def load_data(folder_path):
+def get_data_path_list(data='train'):
+    '''
+    input: 'train' or 'test'  by default is 'train'
+    putput: get a list containing all the training images' paths
+    '''
+    folder_path = 'data/' + data + 'set/png
     assert os.path.exists(folder_path)
     assert os.path.isdir(folder_path)
-    image_path_list = os.listdir(folder_path)
+    return os.listdir(folder_path)
     
